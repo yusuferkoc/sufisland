2177ec9db563e84a39115c2df95208b0d3205cf2
yusuf <yusuferkoc99@gmail.com>
2023-09-16 15:20:43 +0300

    technews

--- pages/works.js
+++ pages/works.js
@@ -7,6 +7,8 @@ import Layout from '../components/layouts/article'
 import sufisland from '../public/static/images/sufisland.png'
 import comp from '../public/static/images/dao.png'
 import f1 from '../public/static/images/1.png'
+import tech1 from '../public/static/images/tech1'
+
 
 
 const Works = () => {
@@ -55,6 +57,16 @@ const Works = () => {
             ></WorkGridItem>
             <Text fontFamily="M PLUS Rounded 1c" fontWeight={'thin'}>
               iOS chat app 
+            </Text>
+          </Section>
+          <Section delay={0.5}>
+            <WorkGridItem
+              id="TechNews"
+              title="TechNews"
+              thumbnail={tech1}
+            ></WorkGridItem>
+            <Text fontFamily="M PLUS Rounded 1c" fontWeight={'thin'}>
+              Hacker News stream app
             </Text>
           </Section>
         </SimpleGrid>
